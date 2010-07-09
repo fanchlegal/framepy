ce00f9f31770ba30f3b18c3aa2a0a0016c696063
Amit Mendapara <mendapara.amit@gmail.com>
2010-07-09 19:24:50 +0530

    Updated version string (0.3).

--- kalapy/release.py
+++ kalapy/release.py
@@ -1,6 +1,6 @@
 """This module defines package release information.
 """
-version         = '0.3b1'
+version         = '0.3'
 license         = 'BSD'
 author          = 'Amit Mendapara'
 author_email    = 'mendapara.amit@gmail.com'
