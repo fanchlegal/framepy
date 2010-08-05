0c3b04b4c3c9fc6a2fffa68c2cf6b9e705c24767
Amit Mendapara <mendapara.amit@gmail.com>
2010-08-05 17:31:58 +0530

    Release 0.4.

--- kalapy/release.py
+++ kalapy/release.py
@@ -1,6 +1,6 @@
 """This module defines package release information.
 """
-version         = '0.3.2'
+version         = '0.4'
 license         = 'BSD'
 author          = 'Amit Mendapara'
 author_email    = 'mendapara.amit@gmail.com'
@@ -8,4 +8,3 @@ description     = 'A highly modular web application framework for Python.'
 copyright       = 'Copyright (c) 2010 Amit Mendapara'
 url             = 'http://www.kalapy.org/'
 download_url    = 'http://www.kalapy.org/downloads.html'
-
